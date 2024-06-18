aa0df69700d7907397a844b2a8c59a12a8f619d8
lelesanmir <leonardodossantosmiranda@gmail.com>
2024-06-18 12:17:49 -0300

    Modified the structure for the project.

--- src/main/java/com/leonardo/helpdesk/resources/TechnicianResource.java
+++ src/main/java/com/leonardo/helpdesk/resources/TechnicianResource.java
@@ -1,12 +1,14 @@
 package com.leonardo.helpdesk.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leonardo.helpdesk.domain.Technician;
+import com.leonardo.helpdesk.domain.dtos.TechnicianDTO;
 import com.leonardo.helpdesk.services.TechnicianService;
 
 @RestController
@@ -17,7 +19,8 @@ public class TechnicianResource {
 	private TechnicianService service;
 
 	@GetMapping(value = "/{id}")
-	public Technician findById(@PathVariable Integer id) {
-		return service.findById(id);
+	public ResponseEntity<TechnicianDTO> findById(@PathVariable Integer id) {
+		Technician obj = service.findById(id);
+		return ResponseEntity.ok().body(new TechnicianDTO(obj));
 	}
 }
