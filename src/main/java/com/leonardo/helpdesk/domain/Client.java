af1e138933e3c5834331548e4da27814132b4b9c
lelesanmir <leonardodossantosmiranda@gmail.com>
2024-07-01 20:02:19 -0300

    Crud for client deployed.

--- src/main/java/com/leonardo/helpdesk/domain/Client.java
+++ src/main/java/com/leonardo/helpdesk/domain/Client.java
@@ -2,17 +2,19 @@ package com.leonardo.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.leonardo.helpdesk.domain.dtos.ClientDTO;
 import com.leonardo.helpdesk.enums.Profile;
 
 @Entity
 public class Client extends Person {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Called> called = new ArrayList<>();
@@ -27,6 +29,17 @@ public class Client extends Person {
 		addProfile(Profile.CLIENT);
 	}
 
+	public Client(ClientDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.name = obj.getName();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.password = obj.getPassword();
+		this.profiles = obj.getProfiles().stream().map(x -> x.getCode()).collect(Collectors.toSet());
+		this.dateCreation = obj.getDateCreation();
+	}
+
 	public List<Called> getCalled() {
 		return called;
 	}
@@ -34,6 +47,5 @@ public class Client extends Person {
 	public void setCalled(List<Called> called) {
 		this.called = called;
 	}
-	
-	
+
 }
