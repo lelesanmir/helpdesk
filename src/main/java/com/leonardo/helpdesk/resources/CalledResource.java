11b21f39ae7aa83ff321fcd1947c3f6c7e004951
lelesanmir <leonardodossantosmiranda@gmail.com>
2024-07-02 17:13:48 -0300

    findById for Called.

---
+++ src/main/java/com/leonardo/helpdesk/resources/CalledResource.java
@@ -0,0 +1,26 @@
+package com.leonardo.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leonardo.helpdesk.domain.Called;
+import com.leonardo.helpdesk.domain.dtos.CalledDTO;
+import com.leonardo.helpdesk.services.CalledService;
+
+@RestController
+@RequestMapping(value = "/called")
+public class CalledResource {
+
+	@Autowired
+	private CalledService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CalledDTO> findById(@PathVariable Integer id) {
+		Called obj = service.findById(id);
+		return ResponseEntity.ok().body(new CalledDTO(obj));
+	}
+}
---
+++ src/main/java/com/leonardo/helpdesk/services/CalledService.java
@@ -0,0 +1,23 @@
+package com.leonardo.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leonardo.helpdesk.domain.Called;
+import com.leonardo.helpdesk.repositories.CalledRepository;
+import com.leonardo.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CalledService {
+
+	
+	@Autowired
+	private CalledRepository repository;
+	
+	public Called findById(Integer id) {
+		Optional<Called> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found! ID: " + id));
+	}
+}
